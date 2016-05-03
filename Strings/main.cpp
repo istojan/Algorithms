c170aa68589cb2fd13fbeaf5c384fe0075d0802a
Stojan Ilieski <stoleilieski@gmail.com>
2016-05-03 13:55:30 +0200

    Add Levenshtein in C++

--- Strings/main.cpp
+++ Strings/main.cpp
@@ -24,6 +24,29 @@ int f[MAXS];
 // GOTO FUNCTION (OR TRIE) IS IMPLEMENTED USING g[][]
 int g[MAXS][MAXC];
 
+int EditDistance(string word1, string word2)
+{
+    int i, j, l1, l2, m;
+    l1 = word1.length();
+    l2 = word2.length();
+    vector< vector<int> > t(l1 + 1, vector<int>(l2 + 1));
+
+    for (i = 0; i <= l1; i++)
+        t[i][0] = i;
+    for (i = 1; i <= l2; i++)
+        t[0][i] = i;
+
+    for (i = 1; i <= l1; i++)
+    {
+        for (j = 1; j <= l2; j++)
+        {
+            m = min(t[i-1][j], t[i][j-1]) + 1;
+            t[i][j] = min(m, t[i-1][j-1] + (word1[i-1] == word2[j-1] ? 0 : 1));
+        }
+    }
+    return t[l1][l2];
+}
+
 // Builds the string matching machine.
 // arr -   array of words. The index of each keyword is important:
 //         "out[state] & (1 << i)" is > 0 if we just found word[i]
@@ -192,7 +215,7 @@ int main()
     string arr[] = {"he", "she", "hers", "his"};
     string text = "ahishers";
     int k = sizeof(arr)/sizeof(arr[0]);
-
+    cout<<EditDistance("baacaabc", "abacbcac")<<"\n";
     searchWords(arr, k, text);
     cout<<ctr<<" words appear";
     return 0;
